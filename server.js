011c6f956dea6077cef7c6dcf0506a0f64b71ec3
Tejashwini BN <tejashwini.bn23@gmail.com>
2025-10-18 17:45:54 +0530

    specific path

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http=require("node:http");
 const server =http.createServer((req,res)=>{
+    if(req.url="/champ"){
+        res.end("hello teju")
+    }
     res.end("hello champ")
 })
 server.listen(3000);
